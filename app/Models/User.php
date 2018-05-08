0b6a30a54087f22aeb5c24fae80152cefba2a4c6
Ningkai Wang <ningkai.wang@foxmail.com>
2018-05-08 10:50:51 +0800

    :bug: fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Notifications\ResetPasswordNotification;
 
 class User extends Authenticatable
 {
