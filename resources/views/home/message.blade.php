a2106398b966dc95c4057a47b07471452eaac4d7
Ningkai Wang <ningkai.wang@foxmail.com>
2018-05-10 21:11:18 +0800

    测试畅言

--- resources/views/home/message.blade.php
+++ resources/views/home/message.blade.php
@@ -14,83 +14,21 @@
                             <div class="hr-line-dashed"></div>
                         </div>
                         <div class="content">
-                            <div class="comments">
-                                <section class="comments-list">
-                                    <!--留言  -->
-                                    @foreach($messages as $message)
-                                        <div class="comment">
-                                            <a href="javascript:void(0)" class="avatar">
-                                                <img src="{{asset('tpl/img/user_avatar.png')}}" alt="{{$message->nickname}}">
-                                            </a>
-                                            <div class="content">
-                                                <div class="pull-right text-muted">{{$message->created_at}}</div>
-                                                <div><a href="javascript:void(0)"><span
-                                                            class="commenter">{{$message->nickname}}</span></a></div>
-                                                <div class="text">{{$message->content}}</div>
-                                            </div>
-                                            @isset($message->reply)
-                                                <div class="comments-list">
-                                                    <!--站长回复留言  -->
-                                                    <div class="comment">
-                                                        <a href="javascript:void(0)" class="avatar">
-                                                            <img src="{{asset('tpl/img/admin_avatar.png')}}" alt="站长">
-                                                        </a>
-                                                        <div class="content">
-                                                            <div
-                                                                class="pull-right text-muted">{{$message->updated_at}}</div>
-                                                            <div><a href="javascript:void(0)"><span
-                                                                        class="commenter">站长</span></a>
-                                                                <span class="text-muted">回复</span>
-                                                                <a href="javascript:void(0)"><span
-                                                                        class="commented">{{$message->nickname}}</span></a>
-                                                            </div>
-                                                            <div class="text">{{$message->reply}}</div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            @endisset
-                                        </div>
-                                    @endforeach
-                                </section>
-                                @include('errors.validator')
-                                <footer>
-                                    <div class="reply-form" id="commentReplyForm">
-                                        <a href="javascript:void(0)" class="avatar"><img src="{{asset('tpl/img/user_avatar.png')}}" alt=""></a>
-                                        <form class="form" id="messageForm" action="{{route('message_store')}}" method="post">
-                                        {{ csrf_field() }}
-                                            <div class="form-group">
-                                                <textarea class="form-control new-comment-text" name="content"
-                                                          id="message" rows="2" placeholder="撰写留言..."></textarea>
-                                            </div>
-                                            <div class="form-group comment-user">
-                                                <div class="row">
-                                                    <div class="col-sm-4">
-                                                        <div class="form-group">
-                                                            <input type="email" name="email" class="form-control"
-                                                                   id="email" placeholder="输入电子邮件（不会在留言显示）*" required>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-sm-4">
-                                                        <div class="form-group">
-                                                            <input type="text" name="nickname" class="form-control"
-                                                                   id="nickname" placeholder="输入评论显示名称 *" required>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-sm-4">
-                                                        <button type="submit"
-                                                                class="btn btn-block btn-outline btn-primary">留言
-                                                        </button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </footer>
-                            </div>
+                            <!--PC和WAP自适应版-->
+                            <div id="SOHUCS" sid="{{$article->id}}" ></div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+@stop
+@section('js')
+<script type="text/javascript">
+(function(){
+var appid = 'cytBgRDT3';
+var conf = 'prod_5531792131296aac07edae47ad9086ee';
+var width = window.innerWidth || document.documentElement.clientWidth;
+if (width < 960) {
+window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="https://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>'); } else { var loadJs=function(d,a){var c=document.getElementsByTagName("head")[0]||document.head||document.documentElement;var b=document.createElement("script");b.setAttribute("type","text/javascript");b.setAttribute("charset","UTF-8");b.setAttribute("src",d);if(typeof a==="function"){if(window.attachEvent){b.onreadystatechange=function(){var e=b.readyState;if(e==="loaded"||e==="complete"){b.onreadystatechange=null;a()}}}else{b.onload=a}}c.appendChild(b)};loadJs("https://changyan.sohu.com/upload/changyan.js",function(){window.changyan.api.config({appid:appid,conf:conf})}); } })(); </script>
 @stop
