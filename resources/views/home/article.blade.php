be37fdc54b7b27264465adec6acd983a2c382242
Ningkai Wang <ningkai.wang@foxmail.com>
2018-06-07 17:45:55 +0800

    测试畅言

--- resources/views/home/article.blade.php
+++ resources/views/home/article.blade.php
@@ -68,6 +68,9 @@
                     </div>
                 </div>
                 <div class="hr-line-dashed"></div>
+                @isset(env('CAHNGYAN_APPID'))
+                <!--PC和WAP自适应版--> <div id="SOHUCS" sid="{{ $article->id }}" ></div>
+                @endisset
             </div>
         </div>
     </div>
@@ -86,4 +89,7 @@
         $('.social-share').share();
     });
 </script>
+@isset()
+<script type="text/javascript">  (function(){  var appid = {{ env('CAHNGYAN_APPID') }};  var conf = {{ env('CAHNGYAN_CONF') }};  var width = window.innerWidth || document.documentElement.clientWidth;  if (width < 960) {  window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="https://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>'); } else { var loadJs=function(d,a){var c=document.getElementsByTagName("head")[0]||document.head||document.documentElement;var b=document.createElement("script");b.setAttribute("type","text/javascript");b.setAttribute("charset","UTF-8");b.setAttribute("src",d);if(typeof a==="function"){if(window.attachEvent){b.onreadystatechange=function(){var e=b.readyState;if(e==="loaded"||e==="complete"){b.onreadystatechange=null;a()}}}else{b.onload=a}}c.appendChild(b)};loadJs("https://changyan.sohu.com/upload/changyan.js",function(){window.changyan.api.config({appid:appid,conf:conf})}); } })(); </script>
+@endisset
 @stop
