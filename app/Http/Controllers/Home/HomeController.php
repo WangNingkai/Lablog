e7fb4e81378b617f5a9469e5c3fbb807fcea14d8
WangNingkai <1655586865@qq.com>
2018-07-13 12:59:23 +0800

    TODO:修改自定义whereMap

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -25,13 +25,10 @@ class HomeController extends Controller
 {
     const CACHE_EXPIRE = 43200;
 
-    public $articleModel;
-
     public $config;
 
-    public function __construct(Article $articleModel)
+    public function __construct()
     {
-        $this->articleModel = $articleModel;
         $this->config = Cache::get('app:config')->toArray();
     }
 
@@ -41,8 +38,7 @@ class HomeController extends Controller
     public function index()
     {
         $articles = Cache::remember('articles:list', self::CACHE_EXPIRE, function () {
-            return $this->articleModel
-                        ->select('id', 'category_id', 'title', 'author', 'description','click', 'created_at')
+            return Article::select('id', 'category_id', 'title', 'author', 'description','click', 'created_at')
                         ->where('status', 1)
                         ->orderBy('created_at', 'desc')
                         ->with(['category', 'tags'])
@@ -57,8 +53,8 @@ class HomeController extends Controller
     public function article($id, Request $request)
     {
         //Redis缓存中没有该article,则从数据库中取值,并存入Redis中,该键值key='article:cache'.$id生命时间5分钟
-        $article = Cache::remember('article:cache:'.$id, self::CACHE_EXPIRE, function () use ($id) {
-            return $this->articleModel->with(['category', 'tags'])->whereId($id)->first();
+        $article = Cache::remember('article:'.$id, self::CACHE_EXPIRE, function () use ($id) {
+            return Article::with(['category', 'tags'])->whereId($id)->first();
         });
 
         if( 0 === $article->status | !is_null($article->deleted_at) ){
@@ -70,9 +66,8 @@ class HomeController extends Controller
         event(new ArticleViewEvent($article, $ip));
 
         // 获取上一篇
-        $prev = Cache::remember('article:cache:pre:'.$id, self::CACHE_EXPIRE, function () use ($id) {
-            return $this->articleModel
-                        ->select('id', 'title')
+        $prev = Cache::remember('article:'.$id.':pre', self::CACHE_EXPIRE, function () use ($id) {
+            return Article::select('id', 'title')
                         ->orderBy('created_at', 'asc')
                         ->where([['id', '>', $id],['status','=',1]])
                         ->limit(1)
@@ -80,9 +75,8 @@ class HomeController extends Controller
         });
 
         // 获取下一篇
-        $next = Cache::remember('article:cache:next:'.$id, self::CACHE_EXPIRE, function () use ($id) {
-            return $this->articleModel
-                        ->select('id', 'title')
+        $next = Cache::remember('article:'.$id.':next', self::CACHE_EXPIRE, function () use ($id) {
+            return Article::select('id', 'title')
                         ->orderBy('created_at', 'desc')
                         ->where([['id', '<', $id],['status','=',1]])
                         ->limit(1)
@@ -99,20 +93,13 @@ class HomeController extends Controller
         $category = Category::findOrFail($id);
         $childCategoryList=Category::where(['pid'=>$id])->get();
 
-        $articles = Cache::remember('article:list:category'.$id, self::CACHE_EXPIRE, function () use ($id) {
-            return $this->articleModel
-                        ->select('id', 'category_id', 'title', 'author', 'description','click', 'created_at')
+        $articles = Cache::remember('category:'.$id.':articles:list', self::CACHE_EXPIRE, function () use ($id) {
+            return Article::select('id', 'category_id', 'title', 'author', 'description','click', 'created_at')
                         ->where(['status'=>1,'category_id'=>$id])
                         ->orderBy('created_at', 'desc')
                         ->with(['category', 'tags'])
                         ->simplePaginate(10);
         });
-        // $articles = $this->articleModel
-        //     ->select('id', 'category_id', 'title', 'author', 'description','click', 'created_at')
-        //     ->where(['status'=>1,'category_id'=>$id])
-        //     ->orderBy('created_at', 'desc')
-        //     ->with(['category', 'tags'])
-        //     ->simplePaginate(10);
         return view('home.category', compact('articles', 'category','childCategoryList'));
     }
 
@@ -124,22 +111,14 @@ class HomeController extends Controller
         $tag = Tag::findOrFail($id);
         $ids = ArticleTag::where('tag_id', $id)->pluck('article_id')->toArray();
 
-        $articles = Cache::remember('article:list:tag'.$id, self::CACHE_EXPIRE, function () use ($ids) {
-            return $this->articleModel
-                        ->select('id', 'category_id', 'title', 'author', 'description','click', 'created_at')
+        $articles = Cache::remember('tag:'.$id.':articles:list', self::CACHE_EXPIRE, function () use ($ids) {
+            return Article::select('id', 'category_id', 'title', 'author', 'description','click', 'created_at')
                         ->where('status',1)
                         ->whereIn('id', $ids)
                         ->orderBy('created_at', 'desc')
                         ->with(['category', 'tags'])
                         ->simplePaginate(10);
         });
-        // $articles = $this->articleModel
-        //     ->select('id', 'category_id', 'title', 'author', 'description','click', 'created_at')
-        //     ->where('status',1)
-        //     ->whereIn('id', $ids)
-        //     ->orderBy('created_at', 'desc')
-        //     ->with(['category', 'tags'])
-        //     ->simplePaginate(10);
         return view('home.tag', compact('articles', 'tag'));
     }
 
@@ -148,24 +127,26 @@ class HomeController extends Controller
      */
     public function archive()
     {
-
-        $archive=$this->articleModel
-            ->select(DB::raw('DATE_FORMAT(created_at, \'%Y-%m\') as time, count(*) as posts'))
-            ->where('status',1)
-            ->groupBy('time')
-            ->orderBy('time','desc')
-            ->simplePaginate(3);
-        foreach ($archive as $v) {
-            $start = date('Y-m-d', strtotime($v->time));
-            $end = date('Y-m-d', strtotime('+1 Month', strtotime($v->time)));
-            $articles = $this->articleModel
-                ->select('id', 'title')
-                ->where('status', 1)
-                ->whereBetween('created_at', [$start, $end])
-                ->orderBy('created_at','desc')
-                ->get();
-            $v->articles = $articles;
+        if( !Cache::has('archive:list') )
+        {
+            $archive = Article::select(DB::raw('DATE_FORMAT(created_at, \'%Y-%m\') as time, count(*) as posts'))
+                            ->where('status',1)
+                            ->groupBy('time')
+                            ->orderBy('time','desc')
+                            ->simplePaginate(3);
+            foreach ($archive as $v) {
+                $start = date('Y-m-d', strtotime($v->time));
+                $end = date('Y-m-d', strtotime('+1 Month', strtotime($v->time)));
+                $articles = Article::select('id', 'title')
+                                ->where('status', 1)
+                                ->whereBetween('created_at', [$start, $end])
+                                ->orderBy('created_at','desc')
+                                ->get();
+                $v->articles = $articles;
+            }
+            Cache::put('archive:list', $archive, self::CACHE_EXPIRE);
         }
+        $archive = Cache::get('archive:list');
         return view('home.archive', compact('archive'));
     }
 
@@ -174,7 +155,9 @@ class HomeController extends Controller
      */
     public function message()
     {
-        $messages=Message::where('status',1)->orderBy('created_at', 'desc')->get();
+        $messages = Cache::remember('messages:list', self::CACHE_EXPIRE, function () use ($ids) {
+            return Message::where('status',1)->orderBy('created_at', 'desc')->get();
+        });
         return view('home.message',compact('messages'));
     }
 
@@ -202,16 +185,15 @@ class HomeController extends Controller
     {
         $keyword = request()->input('keyword');
         $map = [
-            'title' => ['like', '%' . $keyword . '%'],
-            'status' => 1
+            ['title', 'like', '%' . $keyword . '%'],
+            ['status', '=', 1]
         ];
-        $articles = $this->articleModel
-            ->select('id', 'category_id', 'title', 'author', 'description','click', 'created_at')
-            ->whereMap($map)
+        $articles = Article::select('id', 'category_id', 'title', 'author', 'description','click', 'created_at')
+            ->where($map)
             ->orderBy('created_at', 'desc')
             ->with(['category', 'tags'])
             ->simplePaginate(8);
-        $count = $this->articleModel->whereMap($map)->count();
+        $count = Article::where($map)->count();
         $articles->count = $count;
         return view('home.search', compact('articles'));
     }
