57e944098822926e72c1ee1909d716c3895fd837
WangNingkai <1655586865@qq.com>
2018-07-20 09:11:19 +0800

    修改前台主动推送到百度收录

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -279,17 +279,24 @@
     @yield('js')
     <script>
         (function(){
-            var bp = document.createElement('script');
-            var curProtocol = window.location.protocol.split(':')[0];
-            if (curProtocol === 'https'){
-                bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+            //改良版本的百度索引自动推送脚本
+            var canonicalURL, curProtocol;
+            var x=document.getElementsByTagName("link");
+            if(x.length > 0){
+                for (i=0;i<x.length;i++){
+                    if(x[i].rel.toLowerCase() == 'canonical' && x[i].href){
+                        canonicalURL=x[i].href;
+                    }
+                }
+            }
+            if (!canonicalURL){
+                curProtocol = window.location.protocol.split(':')[0];
             }
             else{
-                bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+                curProtocol = canonicalURL.split(':')[0];
             }
-            var s = document.getElementsByTagName("script")[0];
-            s.parentNode.insertBefore(bp, s);
-        })();
+            if (!canonicalURL) canonicalURL = window.location.href;
+            !function(){var e=/([http|https]:\/\/[a-zA-Z0-9\_\.]+\.baidu\.com)/gi,r=canonicalURL,t=document.referrer;if(!e.test(r)){var n=(String(curProtocol).toLowerCase() === 'https')?"https://sp0.baidu.com/9_Q4simg2RQJ8t7jm9iCKT-xh_/s.gif":"//api.share.baidu.com/s.gif";t?(n+="?r="+encodeURIComponent(document.referrer),r&&(n+="&l="+r)):r&&(n+="?l="+r);var i=new Image;i.src=n}}(window);})();
     </script>
 </body>
 
