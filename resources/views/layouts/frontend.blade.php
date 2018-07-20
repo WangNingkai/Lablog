897867a1a635160ffa43c7ff879ad52895489795
WangNingkai <1655586865@qq.com>
2018-07-20 09:07:07 +0800

    添加前台主动推送到百度收录

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -277,7 +277,20 @@
         </script>
     @endif
     @yield('js')
-    <!-- AdminLTE for demo purposes -->
+    <script>
+        (function(){
+            var bp = document.createElement('script');
+            var curProtocol = window.location.protocol.split(':')[0];
+            if (curProtocol === 'https'){
+                bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+            }
+            else{
+                bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+            }
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(bp, s);
+        })();
+    </script>
 </body>
 
 </html>
