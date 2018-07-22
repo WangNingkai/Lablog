b9f2dc2c527b864aa0d91d7662802298e5362c27
WangNingkai <1655586865@qq.com>
2018-07-22 21:05:24 +0800

    fix

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Illuminate\Http\Request;
 use App\Http\Requests\Admin\UpdatePassword;
 use App\Http\Requests\Admin\UpdateProfile;
 use App\Http\Controllers\Controller;
 use App\Models\OauthInfo;
 use App\Models\User as Admin;
-use http\Env\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
