aeef9c630c0154b087be740e3172918ce40b2d52
WangNingkai <1655586865@qq.com>
2018-07-23 16:13:22 +0800

    合并表

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id')->comment('用户id');
             $table->string('name')->comment('用户名');
+            $table->string('avatar')->default('')->comment('用户头像');
             $table->string('email')->unique()->comment('邮箱');
             $table->string('password')->comment('密码');
             $table->rememberToken()->comment('是否记住登录');
