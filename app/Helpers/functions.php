601269db8eb47c7f891ca32cd03ddf0d959134a3
WangNingkai <1655586865@qq.com>
2018-07-24 09:00:31 +0800

    优化时间显示

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -57,32 +57,54 @@ if (!function_exists('operation_event')) {
 
 if (!function_exists('transform_time')) {
     /**
-     * 转换日期或者时间戳为距离现在的时间
-     *
-     * @param $time
-     * @return bool|string
+     * 优化时间显示
+     * @param $sTime
+     * @param int $format
+     * @return false|string
      */
-    function transform_time($time)
+    function transform_time($sTime,$format=0)
     {
         // 如果是日期格式的时间;则先转为时间戳
-        if (!is_integer($time)) {
-            $time = strtotime($time);
+        if (!is_integer($sTime)) {
+            $sTime = strtotime($sTime);
         }
-        $int = time() - $time;
-        if ($int <= 2) {
-            $str = sprintf('刚刚', $int);
-        } elseif ($int < 60) {
-            $str = sprintf('%d秒前', $int);
-        } elseif ($int < 3600) {
-            $str = sprintf('%d分钟前', floor($int / 60));
-        } elseif ($int < 86400) {
-            $str = sprintf('%d小时前', floor($int / 3600));
-        } elseif ($int < 1728000) {
-            $str = sprintf('%d天前', floor($int / 86400));
-        } else {
-            $str = date('Y-m-d H:i', $time);
+        //sTime=源时间，cTime=当前时间，dTime=时间差
+        $cTime        =    time();
+        $dTime        =    $cTime - $sTime;
+        // 计算两个时间之间的日期差
+        $date1 = date_create(date("Y-m-d",$cTime));
+        $date2 = date_create(date("Y-m-d",$sTime));
+        $diff = date_diff($date1,$date2);
+        $dDay = $diff->days;
+
+        if($dTime == 0){
+            return "1秒前";
+        }elseif( $dTime < 60 && $dTime > 0 ){
+            return $dTime."秒前";
         }
-        return $str;
+        elseif( $dTime < 3600 && $dTime > 0 ){
+            return intval($dTime/60)."分钟前";
+        }
+        elseif( $dTime >= 3600 && $dDay == 0 )
+        {
+            return intval($dTime/3600)."小时前";
+        }
+        elseif( $dDay == 1 )
+        {
+            return date("昨天 H:i",$sTime);
+        }
+        elseif( $dDay == 2 )
+        {
+            return date("前天 H:i",$sTime);
+        }
+        elseif($format == 1){
+            return date("m-d H:i",$sTime);
+        }else{
+            if(date('Y',$cTime)!=date('Y',$sTime)) // 不是今年
+                return date("Y-n-j",$sTime);
+            else
+                return date("n-j",$sTime);
+        }
     }
 }
 if (!function_exists('show_message')) {
