8096a8320b67108e3629a82c5024f1be93753938
WangNingkai <1655586865@qq.com>
2018-08-08 20:41:32 +0800

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,9 +10,11 @@ use App\Models\Article;
 use App\Models\Config;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
+
     /**
      * Bootstrap any application services.
      *
@@ -24,33 +26,33 @@ class AppServiceProvider extends ServiceProvider
         view()->composer('*', function ($view) {
             $category_list = Cache::remember('cache:category_list', 1440, function () {
                 // 获取分类导航
-                return Category::select('id', 'name')
+                return Category::query()->select('id', 'name')
                     ->where('pid', 0)
                     ->orderBy('sort', 'asc')
                     ->get();
             });
             $tag_list = Cache::remember('cache:tag_list', 1440, function () {
                 // 获取标签
-                return Tag::select('id', 'name')
+                return Tag::query()->select('id', 'name')
                     ->orderBy('created_at', 'desc')
                     ->get();
             });
             $link_list = Cache::remember('cache:link_list', 1440, function () {
                 // 获取友链
-                return Link::select('id', 'name', 'url')
+                return Link::query()->select('id', 'name', 'url')
                     ->orderBy('sort', 'asc')
                     ->get();
             });
             $top_article_list = Cache::remember('cache:top_article_list', 1440, function () {
                 // 获取热门文章
-                return Article::select('id', 'title')
+                return Article::query()->select('id', 'title')
                     ->orderBy('click', 'desc')
                     ->limit(10)
                     ->get();
             });
             $config = Cache::remember('cache:config', 1440, function () {
                 // 获取置顶文章
-                return Config::pluck('value', 'name');
+                return Config::query()->pluck('value', 'name');
             });
             // 分配数据
             $assign = compact('category_list', 'tag_list', 'top_article_list', 'link_list','config');
