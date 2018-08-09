6c03d5a5923965e7aed7d490a0eea9c32180ef92
WangNingkai <1655586865@qq.com>
2018-08-09 16:20:47 +0800

    初始化单页模块

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Page extends Model
 {
     use SoftDeletes;
-    # 单页模块
+    # 单页模块 存放关于页面等单一页面。友链也可以设置进去
     const STATUS_DISPLAY = 1;
     const STATUS_HIDE = 0;
 }
