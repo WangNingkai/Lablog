548c03eea800cb0b0361a7c8d43345f710ef5311
WangNingkai <1655586865@qq.com>
2018-08-24 22:38:31 +0800

    fix

--- app/Http/Controllers/Auth/HookController.php
+++ app/Http/Controllers/Auth/HookController.php
@@ -3,12 +3,14 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class HookController extends Controller
 {
     public function push(Request $request,$type)
     {
-        $data = $request->all();
+        $data = $request->getContent();
         if ($type =='gitee') {
             $allow = $request->header('X-Gitee-Token') === env('GITEE_HOOK_PASSWORD') ?:false;
         } elseif ($type =='gogs') {
@@ -16,7 +18,16 @@ class HookController extends Controller
             $hash = hash_hmac ('sha256',$data,env('GOGS_HOOK_PASSWORD'));
             $allow = $signature === $hash?:false;
         }
-        if ($allow)
-            shell_exec('cd '.base_path().' && git pull && composer install');
+        if ($allow) {
+            $command = 'bash /root/update-blog.sh';
+            $process = new Process($command);
+            if (!$process->isSuccessful()) {
+                throw new ProcessFailedException($process);
+            }
+            echo $process->getOutput();
+        }
+        return abort(403);
+
+
     }
 }
