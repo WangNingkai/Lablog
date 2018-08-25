12ddb0b8553739045d6359be0c545d22b731f68c
WangNingkai <1655586865@qq.com>
2018-08-25 13:57:36 +0800

    fix Hook执行动作

--- app/Console/Commands/UpdatePush.php
+++ app/Console/Commands/UpdatePush.php
@@ -40,11 +40,11 @@ class UpdatePush extends Command
         $basePath =base_path();
         $command = "sudo nohup /usr/bin/bash /root/blog.sh update {$basePath} >> /root/push.log 2>&1 &";
         $this->info('[' . date('Y-m-d H:i:s', time()) . '] =====执行命令=====');
-        $this->info('[' . date('Y-m-d H:i:s', time()) . '] [' . $command . ']');
-        $this->info('[' . date('Y-m-d H:i:s', time()) . '] ==========');
         $process = new Process($command);
         $process ->run();
-        $result =  $process->isSuccessful();
+        $this->info('[' . date('Y-m-d H:i:s', time()) . '] [' . $command . ']');
+        $result = $process->isSuccessful();
+        $this->info('[' . date('Y-m-d H:i:s', time()) . '] =====执行完毕=====');
         $this->info('[' . date('Y-m-d H:i:s', time()) . '] 执行结果：'.$result);
         $this->info('[' . date('Y-m-d H:i:s', time()) . '] 输出结果：'.$process->getOutput());
 
